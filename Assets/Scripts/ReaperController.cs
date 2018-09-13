0bef7292bb3faddf5fe80855549a5a8f9265bb54
Mathieu Bédard <mthb54@live.ca>
2018-09-13 14:53:22 -0400

    test

---
+++ Assets/Scripts/ReaperController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReaperController : MonoBehaviour ,IPlayer{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Lights")
+		{
+			
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Darkness")
+		{
+			
+		}
+	}
+}
