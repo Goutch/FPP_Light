a55117c01dd9c060984590eccf391aa0d10ab871
Gabriel Bouchard <gaboutch@outlook.com>
2018-09-13 13:36:45 -0400

    player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float jumpForce;
+	
+	[SerializeField] private float speed;
+
+	private bool jumping=false;
+	private Rigidbody2D rigidbody;
+	// Use this for initialization
+	void Start ()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Move(int dir)
+	{
+		transform.Translate(Vector2.right*dir*Time.deltaTime);
+	}
+
+	public void Jump()
+	{
+		rigidbody.AddForce(Vector2.up*jumpForce,ForceMode2D.Impulse);
+	}
+}
---
+++ Assets/Scripts/PlayerInputs.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputs : MonoBehaviour
+{
+    [SerializeField] private KeyCode moveLeftKey;
+
+    [SerializeField] private KeyCode moveRightKey;
+
+    [SerializeField] private KeyCode jumpKey;
+
+    private PlayerController player;
+
+    private void Start()
+    {
+        player = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(jumpKey))
+        {
+            player.Jump();
+        }
+
+        if (Input.GetKey(moveRightKey))
+        {
+            player.Move(1);
+        }
+
+        if (Input.GetKey(moveLeftKey))
+        {
+            player.Move(-1);
+        }
+    }
+}
